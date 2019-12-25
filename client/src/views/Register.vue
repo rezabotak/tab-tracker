a877e98ce1786884a7880d0d320ff2152350ac48
rezabotak <rezaarakhman@gmail.com>
2019-12-25 17:22:31 +0700

    setting up registration page

---
+++ client/src/views/Register.vue
@@ -0,0 +1,37 @@
+/* eslint-disable prettier/prettier */
+<template>
+  <div>
+    <h1>Register</h1>
+    <input type="email" name="email" placeholder="email" v-model="email" />
+    <br />
+    <input
+      type="password"
+      name="password"
+      placeholder="password"
+      v-model="password"
+    />
+    <br />
+    <button @click="register">Register</button>
+  </div>
+</template>
+
+<script>
+import AuthenticationService from "@/services/AuthenticationService";
+
+export default {
+  data() {
+    return {
+      email: "",
+      password: ""
+    };
+  },
+  methods: {
+    async register() {
+      await AuthenticationService.register({
+        email: this.email,
+        password: this.password
+      });
+    }
+  }
+};
+</script>
